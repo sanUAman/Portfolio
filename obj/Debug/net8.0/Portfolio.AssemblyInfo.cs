80627f04bd62a0d179c43c29d1d502ae7e7ab934
Oleksandr Dzhivalov <dzivalovoleksandr@gmail.com>
2025-01-20 14:16:17 +0200

    Added links in btns.

--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+472831e47a66caf3263a5b02fcae30683ce1ebcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a573fad4fc501383efa02e04027747c7fadbb5a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
