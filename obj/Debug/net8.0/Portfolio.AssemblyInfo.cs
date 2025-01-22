acf3ec8d0fb984fed3386eeacc50175e391a9ea0
Oleksandr Dzhivalov <dzivalovoleksandr@gmail.com>
2025-01-22 22:26:07 +0200

    Added burger-button and menu.

--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a573fad4fc501383efa02e04027747c7fadbb5a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80627f04bd62a0d179c43c29d1d502ae7e7ab934")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ wwwroot/js/smooth_animations.js
@@ -0,0 +1,67 @@
+﻿document.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+document.querySelector('.check-projects-btn').addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({
+        top: 2000,
+        behavior: 'smooth',
+    });
+});
+document.querySelector('.back-to-about-btn').addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({
+        top: 950,
+        behavior: 'smooth',
+    });
+});
+document.querySelector('.back-to-projects-btn').addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({
+        top: 2000,
+        behavior: 'smooth',
+    });
+});
+document.querySelector('.back-to-contacts-btn').addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({
+        top: 3300,
+        behavior: 'smooth',
+    });
+});
+document.querySelector('.back-to-top-btn').addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+});
+
+AOS.init({
+    once: true
+});
+
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav');
+
+// Додаємо подію на кнопку-бургер
+menuToggle.addEventListener('click', () => {
+    if (nav.classList.contains('open')) {
+        nav.classList.remove('open'); // Закриваємо меню
+    } else {
+        nav.classList.add('open'); // Відкриваємо меню
+    }
+});
+
+// Скидаємо клас 'open', коли ширина екрану більше 750px
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 750) {
+        nav.classList.remove('open'); // Видаляємо клас 'open'
+    }
+});
+
