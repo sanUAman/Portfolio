f99896e7963cb5f17327db89dba292f977a91e9e
Oleksandr Dzhivalov <dzivalovoleksandr@gmail.com>
2025-02-20 14:32:06 +0200

    Full web-portfolio.

--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75d47403b1bbf6f30369c0d9aec372314ea2b8af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f95c9ecacafbefa6dd55087753247f829e996d68")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- wwwroot/js/smooth_animations.js
+++ wwwroot/js/smooth_animations.js
@@ -1,4 +1,15 @@
-﻿document.addEventListener('scroll', function () {
+﻿function scrollToElementWithOffset(elementId, offset) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        const elementPosition = element.getBoundingClientRect().top + window.scrollY;
+        window.scrollTo({
+            top: elementPosition - offset,
+            behavior: 'smooth'
+        });
+    }
+}
+
+document.addEventListener('scroll', function () {
     const header = document.querySelector('header');
     if (window.scrollY > 50) {
         header.classList.add('scrolled');
@@ -6,73 +17,51 @@
         header.classList.remove('scrolled');
     }
 });
+
 document.querySelector('.check-projects-btn').addEventListener('click', function (e) {
     e.preventDefault();
-    window.scrollTo({
-        top: 2000,
-        behavior: 'smooth',
-    });
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth <= 813) {
+        scrollToElementWithOffset('projects', -150);
+    } else if (screenWidth >= 814) {
+        scrollToElementWithOffset('projects', -275);
+    }
 });
+
 document.querySelector('.back-to-about-btn').addEventListener('click', function (e) {
     e.preventDefault();
-
     const screenWidth = window.innerWidth;
 
-    if (screenWidth <= 647) {
-        window.scrollTo({
-            top: 900,
-            behavior: 'smooth',
-        });
-    } else if (screenWidth <= 1920) {
-        window.scrollTo({
-            top: 950,
-            behavior: 'smooth',
-        });
+    if (screenWidth <= 813) {
+        scrollToElementWithOffset('about', -50);
+    } else if (screenWidth >= 814) {
+        scrollToElementWithOffset('about', -75);
     }
 });
+
 document.querySelector('.back-to-projects-btn').addEventListener('click', function (e) {
     e.preventDefault();
-
     const screenWidth = window.innerWidth;
 
-    if (screenWidth <= 490) {
-        window.scrollTo({
-            top: 2150,
-            behavior: 'smooth',
-        });
-    } else if (screenWidth <= 1920) {
-        window.scrollTo({
-            top: 2000,
-            behavior: 'smooth',
-        });
+    if (screenWidth <= 813) {
+        scrollToElementWithOffset('projects', -150);
+    } else if (screenWidth >= 814) {
+        scrollToElementWithOffset('projects', -275);
     }
 });
+
 document.querySelector('.back-to-contacts-btn').addEventListener('click', function (e) {
     e.preventDefault();
     const screenWidth = window.innerWidth;
 
-    if (screenWidth <= 489) {
-        window.scrollTo({
-            top: 3525,
-            behavior: 'smooth',
-        });
-    } else if (screenWidth <= 683) {
-        window.scrollTo({
-            top: 3800,
-            behavior: 'smooth',
-        });
-    } else if (screenWidth <= 813) {
-        window.scrollTo({
-            top: 3725,
-            behavior: 'smooth',
-        });
-    } else if (screenWidth <= 1920) {
-        window.scrollTo({
-            top: 3200,
-            behavior: 'smooth',
-        });
+    if (screenWidth <= 813) {
+        scrollToElementWithOffset('contacts', -150);
+    } else if (screenWidth >= 814) {
+        scrollToElementWithOffset('contacts', -275);
     }
 });
+
 document.querySelector('.back-to-top-btn').addEventListener('click', function (e) {
     e.preventDefault();
     window.scrollTo({
@@ -91,18 +80,17 @@ const nav = document.querySelector('nav');
 // Додаємо подію на кнопку-бургер
 menuToggle.addEventListener('click', () => {
     if (nav.classList.contains('open')) {
-        nav.classList.remove('open'); // Закриваємо меню
-        menuToggle.textContent = "☰"; // Міняємо іконку на бургер
+        nav.classList.remove('open');
+        menuToggle.textContent = "☰";
     } else {
-        nav.classList.add('open'); // Відкриваємо меню
-        menuToggle.textContent = "✖"; // Міняємо іконку на хрестик
+        nav.classList.add('open');
+        menuToggle.textContent = "✖";
     }
 });
 
-// Скидаємо клас 'open', коли ширина екрану більше 750px
 window.addEventListener('resize', () => {
     if (window.innerWidth > 750) {
-        nav.classList.remove('open'); // Видаляємо клас 'open'
-        menuToggle.textContent = "☰"; // Міняємо іконку на бургер
+        nav.classList.remove('open');
+        menuToggle.textContent = "☰";
     }
 });
